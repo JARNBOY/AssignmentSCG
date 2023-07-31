2ee5ce286861393429df1f40c324d26dce67fbd6
Papon <papon.s@lawyerplusgroup.com>
2023-07-31 15:46:54 +0700

    Initial Commit

---
+++ AssignmentSCG/AssignmentSCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentSCGApp.swift
+//  AssignmentSCG
+//
+//  Created by Papon Supamongkonchai on 31/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentSCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
